ab0c6536c13c20eeb99279e86ac60737e3491c60
Anthwam <anthwam13@outlook.com>
2021-04-15 12:17:55 -0400

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "./features/store";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>React Pokédex</title>
+        <meta
+          name="description"
+          content="a simple pokédex for your pokemon needs."
+        />
+      </Helmet>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
